765cdd953f8f7c59e9104053b8eebfdb2d498cad
udaya2208 <udayakumarsundar@gmail.com>
2019-11-07 00:15:50 +0530

    db changes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 //require('dotenv').config(); 
-mongoose.connect(mongoURI,{useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.mongoURI,{useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
 app.use(express.json());
 const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter)
-app.listen(PORT, () => console.log('server started'+PORT));
+app.listen(process.env.PORT, () => console.log('server started'+process.env.PORT));
