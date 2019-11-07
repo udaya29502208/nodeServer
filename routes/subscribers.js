1d200957afc5e7153bc3179d5e611971319cf05d
udaya2208 <udayakumarsundar@gmail.com>
2019-11-07 22:43:52 +0530

    db changes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const Subscriber = require('../models/subscriber')
+//const Subscriber = require('../models/subscriber')
 // Get all subscribers
 router.get('/', async (req, res) => {
-    try {
-      const subscribers = await Subscriber.find()
-      res.json(subscribers)
-    } catch (err) {
-      res.status(500).json({ message: err.message })
-    }
+    // try {
+    //   const subscribers = await Subscriber.find()
+    //   res.json(subscribers)
+    // } catch (err) {
+    //   res.status(500).json({ message: err.message })
+    // }
   })
 
 // Get one subscriber
@@ -17,58 +17,59 @@ router.get('/:id',getSubscriber, (req, res) => {
 
 // Create one subscriber
 router.post('/', async (req, res) => {
-    const subscriber = new Subscriber({
-      name: req.body.name,
-      subscribedChannel: req.body.subscribedChannel
-    })
+    // const subscriber = new Subscriber({
+    //   name: req.body.name,
+    //   subscribedChannel: req.body.subscribedChannel
+    // })
   
-    try {
-      const newSubscriber = await subscriber.save()
-      res.status(201).json(newSubscriber)
-    } catch (err) {
-      res.status(400).json({ message: err.message })
-    }
+    // try {
+    //   const newSubscriber = await subscriber.save()
+    //   res.status(201).json(newSubscriber)
+    // } catch (err) {
+    //   res.status(400).json({ message: err.message })
+    // }
   })
 
 // Update one subscriber
 router.patch('/:id', getSubscriber, async (req, res) => {
-    if (req.body.name != null) {
-      res.subscriber.name = req.body.name
-    }
+    // if (req.body.name != null) {
+    //   res.subscriber.name = req.body.name
+    // }
   
-    if (req.body.subscribedChannel != null) {
-      res.subscriber.subscribedChannel = req.body.subscribedChannel
-    }
-    try {
-      const updatedSubscriber = await res.subscriber.save()
-      res.json(updatedSubscriber)
-    } catch {
-      res.status(400).json({ message: err.message })
-    }
+    // if (req.body.subscribedChannel != null) {
+    //   res.subscriber.subscribedChannel = req.body.subscribedChannel
+    // }
+    // try {
+    //   const updatedSubscriber = await res.subscriber.save()
+    //   res.json(updatedSubscriber)
+    // } catch {
+    //   res.status(400).json({ message: err.message })
+    // }
   
   })
 
 // Delete one subscriber
 router.delete('/:id', getSubscriber, async (req, res) => {
-    try {
-      await res.subscriber.remove()
-      res.json({ message: 'Deleted This Subscriber' })
-    } catch(err) {
-      res.status(500).json({ message: err.message })
-    }
+    // try {
+    //   await res.subscriber.remove()
+    //   res.json({ message: 'Deleted This Subscriber' })
+    // } catch(err) {
+    //   res.status(500).json({ message: err.message })
+    // }
   })
 
 async function getSubscriber(req, res, next) {
-    try {
-      subscriber = await Subscriber.findById(req.params.id)
-      if (subscriber == null) {
-        return res.status(404).json({ message: 'Cant find subscriber'})
-      }
-    } catch(err){
-      return res.status(500).json({ message: err.message })
-    }
+    // try {
+    //   subscriber = await Subscriber.findById(req.params.id)
+    //   if (subscriber == null) {
+    //     return res.status(404).json({ message: 'Cant find subscriber'})
+    //   }
+    // } catch(err){
+    //   return res.status(500).json({ message: err.message })
+    // }
   
-    res.subscriber = subscriber
+    // res.subscriber = subscriber
+    res.subscriber = {"result":"success"}
     next()
   }
 module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 //require('dotenv').config(); 
-mongoose.connect(process.env.mongoURI,{useUnifiedTopology: true, useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('connected to database'))
+//mongoose.connect(process.env.mongoURI,{useUnifiedTopology: true, useNewUrlParser: true });
+//const db = mongoose.connection;
+//db.on('error', (error) => console.error(error))
+//db.once('open', () => console.log('connected to database'))
 app.use(express.json());
 const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter)
-app.listen(27017, () => console.log('server started'+"5000"));
+app.listen(5000, () => console.log('server started'+"5000"));
