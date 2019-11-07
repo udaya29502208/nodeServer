e349e73d0cc45bb09fbc267af683cd951adf54dd
udaya2208 <udayakumarsundar@gmail.com>
2019-11-07 22:33:34 +0530

    db changes

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ db.once('open', () => console.log('connected to database'))
 app.use(express.json());
 const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter)
-app.listen(5000, () => console.log('server started'+"5000"));
+app.listen(27017, () => console.log('server started'+"5000"));
